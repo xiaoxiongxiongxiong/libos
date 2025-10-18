a1020b151d65db5f7aa756da371c1a9856623ccc
xiaoxiongxiongxiong <981303787@qq.com>
2025-10-18 15:38:03 +0800

    搭建红黑树框架

---
+++ include/os_rbt.h
@@ -0,0 +1,111 @@
+﻿#ifndef __OS_RBT_H__
+#define __OS_RBT_H__
+
+#include "libos.h"
+
+typedef struct _os_rbt_t os_rbt_t;
+typedef struct _os_rbt_node_t os_rbt_node_t;
+
+OS_API_BEGIN
+
+/*
+* @brief  键值比较回调函数
+* @param  key2
+* @param  key1
+* @return -1,0,1
+*/
+typedef int(*os_rbt_compare)(const void * key1, const void * key2);
+
+/*
+* @brief  key/val释放函数
+* @param  data
+*/
+typedef void(*os_rbt_free)(void * data);
+
+/*
+* os_rbt_create
+* @brief  创建红黑树
+* @param  cmp  键值比较函数
+* @param  key  key释放函数
+* @param  val  val释放函数
+* @return NULL/实例
+*/
+os_rbt_t * os_rbt_create(os_rbt_compare cmp, os_rbt_free key, os_rbt_free val);
+
+/*
+* os_rbt_destroy
+* @brief  销毁红黑树
+* @param  rbt  树实例
+*/
+void os_rbt_destroy(os_rbt_t ** rbt);
+
+/*
+* os_rbt_clear
+* @brief  清空树
+* @param  rbt  树实例
+*/
+void os_rbt_clear(os_rbt_t * rbt);
+
+/*
+* os_rbt_insert
+* @brief  插入数据
+* @param  rbt  树实例
+* @param  key  要插入的键
+* @param  val  要插入的值
+* @return true/false
+*/
+bool os_rbt_insert(os_rbt_t * rbt, void * key, void * val);
+
+/*
+* os_rbt_erase
+* @brief  删除某个节点
+* @param  rbt  树实例
+* @param  key  要删除的键
+* @return true/false
+*/
+bool os_rbt_erase(os_rbt_t * rbt, const void * key);
+
+/*
+* os_rbt_find
+* @brief  查找某个节点
+* @param  rbt  树实例
+* @param  key  要查找的键
+* @return NULL/节点
+*/
+os_rbt_node_t * os_rbt_find(const os_rbt_t * rbt, const void * key);
+
+/*
+* os_rbt_key
+* @brief  获取kek值
+* @param  node  节点
+* @return NULL/key
+*/
+void * os_rbt_key(const os_rbt_node_t * node);
+
+/*
+* os_rbt_val
+* @brief  获取val值
+* @param  node  节点
+* @return NULL/val
+*/
+void * os_rbt_val(const os_rbt_node_t * node);
+
+/*
+* os_rbt_size
+* @brief  获取树元素个数
+* @param  rbt  树实例
+* @return 元素个数
+*/
+size_t os_rbt_size(const os_rbt_t * rbt);
+
+/*
+* os_rbt_empty
+* @brief  判断树是否为空
+* @param  rbt  树实例
+* @return true/false
+*/
+bool os_rbt_empty(const os_rbt_t * rbt);
+
+OS_API_END
+
+#endif
---
+++ src/tree/os_rbt.c
@@ -0,0 +1,121 @@
+﻿#include "os_rbt.h"
+
+#include <string.h>
+#include <malloc.h>
+
+typedef enum _OS_RBT_COLOR_TYPE
+{
+	OS_RBT_COLOR_RED,
+	OS_RBT_COLOR_BLACK
+} OS_RBT_COLOR_TYPE;
+
+struct _os_rbt_node_t {
+	void * key;
+	void * val;
+	OS_RBT_COLOR_TYPE color;
+	struct _os_rbt_node_t * parent;
+	struct _os_rbt_node_t * left;
+	struct _os_rbt_node_t * right;
+};
+
+struct _os_rbt_t {
+	size_t size;
+	os_rbt_node_t * root;
+	os_rbt_node_t * nil;
+	os_rbt_compare cmp;
+	os_rbt_free key_free;
+	os_rbt_free val_free;
+};
+
+// 创建一个节点
+static os_rbt_node_t * os_rbt_new_node(const os_rbt_node_t * parent, const os_rbt_node_t * left, const os_rbt_node_t * right,
+									   OS_RBT_COLOR_TYPE color, const void * key, const void * val);
+// 左旋转
+static void os_rbt_left_rotate(os_rbt_t * rbt);
+// 右旋转
+static void os_rbt_right_rotate(os_rbt_t * rbt);
+
+os_rbt_t * os_rbt_create(os_rbt_compare cmp, os_rbt_free key, os_rbt_free val)
+{
+	os_rbt_t * rbt = (os_rbt_t *)calloc(1, sizeof(os_rbt_t));
+	if (NULL == rbt)
+		return NULL;
+
+	rbt->cmp = cmp;
+	rbt->key_free = key;
+	rbt->val_free = val;
+
+	return rbt;
+}
+
+void os_rbt_destroy(os_rbt_t ** rbt)
+{
+
+}
+
+void os_rbt_clear(os_rbt_t * rbt)
+{
+
+}
+
+bool os_rbt_insert(os_rbt_t * rbt, void * key, void * val)
+{
+	return true;
+}
+
+bool os_rbt_erase(os_rbt_t * rbt, const void * key)
+{
+	return true;
+}
+
+os_rbt_node_t * os_rbt_find(const os_rbt_t * rbt, const void * key)
+{
+	return NULL;
+}
+
+void * os_rbt_key(const os_rbt_node_t * node)
+{
+	return node ? node->key : NULL;
+}
+
+void * os_rbt_val(const os_rbt_node_t * node)
+{
+	return node ? node->val : NULL;
+}
+
+size_t os_rbt_size(const os_rbt_t * rbt)
+{
+	return rbt ? rbt->size : 0u;
+}
+
+bool os_rbt_empty(const os_rbt_t * rbt)
+{
+	return rbt ? 0 == rbt->size : true;
+}
+
+os_rbt_node_t * os_rbt_new_node(const os_rbt_node_t * parent, const os_rbt_node_t * left, const os_rbt_node_t * right,
+								OS_RBT_COLOR_TYPE color, const void * key, const void * val)
+{
+	os_rbt_node_t * node = (os_rbt_node_t *)malloc(sizeof(os_rbt_node_t));
+	if (NULL == node)
+		return NULL;
+
+	node->key = key;
+	node->val = val;
+	node->color = color;
+	node->parent = parent;
+	node->left = left;
+	node->right = right;
+
+	return node;
+}
+
+void os_rbt_left_rotate(os_rbt_t * rbt)
+{
+
+}
+
+void os_rbt_right_rotate(os_rbt_t * rbt)
+{
+
+}
